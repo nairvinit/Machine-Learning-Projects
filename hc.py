f28dee7006fd38dbaa661dfdb91f71b83809a631
Vinit Nair <42565325+nairvinit@users.noreply.github.com>
2018-11-22 10:55:04 +0530

    Update hc.py

--- hc.py
+++ hc.py
@@ -8,7 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
-# y = dataset.iloc[:, 3].values
+
 
 # Using the dendrogram to find the optimal number of clusters
 import scipy.cluster.hierarchy as sch
